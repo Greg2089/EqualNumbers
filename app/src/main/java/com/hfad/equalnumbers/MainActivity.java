33b279c5bc5edf111afcaf1b968727055ebbcfdf
admin <g.lyalko@mail.ru>
2022-08-06 16:59:16 +0300

    2) Begin activity

--- app/src/main/java/com/hfad/equalnumbers/MainActivity.java
+++ app/src/main/java/com/hfad/equalnumbers/MainActivity.java
@@ -3,6 +3,10 @@ package com.hfad.equalnumbers;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MyLog", "");
+
+        //region Получение ссылок на объекты из макета
+
+        EditText firstET = findViewById(R.id.firstET);
+        EditText secondET = findViewById(R.id.secondET);
+        Button button = findViewById(R.id.button);
+        //endregion
+
+        //region Обработка нажатия кнопки
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("MyLog", "OnClick");
+            }
+        });
+
+        //endregion
+
     }
 }
